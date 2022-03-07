9abb5594b803d00038d968487637a59a1459d9fd
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2022-03-07 14:22:44 +0100

    coolstorybro

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBD_Compulsory
+{
+    public class Config
+    {
+        public string username = "sa";
+        public string password = "Warstroy21!";
+    }
+}
--- Db.cs
+++ Db.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,35 @@ namespace DBD_Compulsory
 {
     public class Db
     {
+        private string connectionString;
+        SqlConnection connection;
+        SqlCommand command;
+
+        public Db()
+        {
+            connectionString = $"Server=localhost;Database=Company;User Id=sa;Password=Warstroy21!;";
+        }
+
         public object USP_CreateDepartment(string Dname, string MgrSSN)
         {
-            return ":)";
+            connection = new SqlConnection(connectionString);
+
+            string sql = $"EXECUTE usp_CreateDepartment '{Dname}', {MgrSSN};";
+
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+                int identity = Convert.ToInt32(command.ExecuteScalar());
+                command.Dispose();
+                connection.Close();
+                return identity;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
     }
 }
