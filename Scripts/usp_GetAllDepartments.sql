783559bdd3f0a2f69378cf9641f0569dd88d541c
René Jørgensen <rene686f@easv365.dk>
2022-03-14 12:18:25 +0100

    Update usp_GetAllDepartments.sql

--- Scripts/usp_GetAllDepartments.sql
+++ Scripts/usp_GetAllDepartments.sql
@@ -3,8 +3,9 @@ AS
 BEGIN
 	BEGIN
 		SET NOCOUNT ON;
-		SELECT Department.DNumber, Department.DName, COUNT(Employee.Dno) AS EmpCount FROM Employee
-		INNER JOIN Department ON Department.DNumber = Employee.Dno
+		SELECT Department.DNumber, Department.DName, COUNT(Employee.Dno) AS EmpCount 
+			FROM Department
+		LEFT JOIN Employee ON Department.DNumber = Employee.Dno
 		GROUP BY Department.DNumber, Department.DName
 		END
 END
